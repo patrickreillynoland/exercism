fa51cef775795ba98c3e95feb2c39e098f5799ba
Patrick Noland <patrickreillynoland@gmail.com>
2018-01-18 17:55:04 -0800

    Update leap.js

--- leap/leap.js
+++ leap/leap.js
@@ -1,15 +1,7 @@
-//
-// This is only a SKELETON file for the "Leap" exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-
-//Assign the input value to this particular object
 var Year = function(input) {
     this.yr = input;
 };
 
-//Returns a "truthy" or "falsy" statement
 Year.prototype.isLeap = function() {
     return this.yr % 4 == 0 && (this.yr % 100 !== 0 || this.yr % 400 === 0);
 };
